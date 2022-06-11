55713aed2da4ecb33f3f146d227888f7ad6872ea
Trung Huỳnh <19520317@ms.uit.edu.vn>
2022-06-11 23:11:49 +0700

    feat: update company

--- routes/merchant/auth.routes.js
+++ routes/merchant/auth.routes.js
@@ -4,5 +4,7 @@ const authRouter = require("express").Router();
 authRouter.post("/signup", authController.signUp);
 authRouter.post("/login", authController.login);
 authRouter.post("/verify", authController.verify);
+authRouter.patch("/password/:idCompany", authController.changePassword);
+authRouter.patch("/:idCompany", authController.updateCompany);
 
 module.exports = authRouter;
